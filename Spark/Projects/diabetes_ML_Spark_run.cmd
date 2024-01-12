64e36609d29644dc86f5b30883c8909fd6576d23
Pavlo_Miroshnyk <pv.miroshnyk@gmail.com>
2024-01-12 10:11:21 +0100

    Spark Project (diabetes) v1.1

--- Spark/Projects/diabetes_ML_Spark_run.cmd
+++ Spark/Projects/diabetes_ML_Spark_run.cmd
@@ -1,2 +1,6 @@
-rem $env:PYSPARK_PYTHON = "C:\Users\Pavlo\AppData\Local\Programs\Python\Python311\python.exe"
-spark-submit diabetes_ML_Spark.py
+rem script for starting
+rem the "Dlog4j.level=WARN" options suppress redundant messages
+spark-submit ^
+  --conf "spark.driver.extraJavaOptions=-Dlog4j.level=WARN" ^
+  --conf "spark.executor.extraJavaOptions=-Dlog4j.level=WARN" ^
+  diabetes_ML_Spark.py
